752607c5a6b3598e7428a02e59d16616791ea188
Austin Martinez <austinm@cpp.edu>
2021-12-01 21:33:13 -0800

    Added Horizontal scrolling

--- kilo.c
+++ kilo.c
@@ -41,10 +41,12 @@ typedef struct erow {
 
 struct editorConfig {
     int cx, cy;
+    int rowoff;
+    int coloff;
     int screenrows;
     int screencols;
     int numrows;
-    erow row;
+    erow *row;
     struct termios orig_termios;
 };
 
@@ -162,6 +164,19 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
+/*** row opertions ***/
+
+void editorAppendRow(char *s, size_t len) {
+    E.row = realloc(E.rows, sizeof(erow) * (E.numrows + 1));
+
+    int at = E.numrows;
+    E.row[at].size = len;
+    E.row[at].chars = malloc(len + 1);
+    memcpy(E.row[at].chars, s, len);
+    E.row[at].chars[len] = '\0';
+    E.numrows++;
+}
+
 /*** file i/o ***/
 
 void editorOpen(char *filename) {
@@ -173,15 +188,11 @@ void editorOpen(char *filename) {
     size_t linecap = 0;
     ssize_t linelen;
     linelen = getline(&line, &linecap, fp);
-    if (linelen != -1) {
+    while ((linelen = getline(&line, &linecap, fp)) != -1) {
         while (linelen > 0 && (line[linelen - 1] == '\n' ||
                                line[linelen - 1] == '\r'))
             linelen--;
-        E.row.size = linelen;
-        E.row.chars = malloc(linelen + 1);
-        memcpy(E.row.chars, line, linelen);
-        E.row.chars[linelen] = '\0';
-        E.numrows = 1;
+        editorAppendRow(line, linelen);
     }
     free(line);
     fclose(fp);
@@ -211,11 +222,27 @@ void abFree(struct abuf *ab) {
 
 /*** output ***/
 
+void editorScroll() {
+    if (E.cy < E.rowoff) {
+        E.rowoff = E.cy;
+    }
+    if (E.cy >= E.rowoff + E.screenrows) {
+        E.rowoff = E.cy - E.screenrows + 1;
+    }
+    if (E.cx < E.coloff) {
+        E.coloff = E.cx;
+    }
+    if (E.cx >= E.coloff + E.screencols) {
+        E.coloff = E.cx - E.screencols + 1;
+    }
+}
+
 void editorDrawRows(struct abuf *ab) {
-    int y
+    int y;
     for (y = 0; y < E.screenrows; y++) {
-        if (y >= E.numrows) {
-            if (y == E.screenrows / 3) {
+        int filerow = y + E.rowoff;
+        if (filerow >= E.numrows) {
+          if (E.numrows == 0 && y == E.screenrows / 3) {
             char welcome[80];
             int welcomelen = snprintf(welcome, sizeof(welcome),
                 "Kilo editor -- version %s", KILO_VERSION);
@@ -231,9 +258,10 @@ void editorDrawRows(struct abuf *ab) {
             abAppend(ab, '~', 1);
            }
         } else {
-            int len = E.row.size;
+            int len = E.row[filerow].size - E.coloff;
+            if (len < 0) len = 0;
             if (len > E.screencols) len = E.screencols;
-            abAppend(ab, E.row.chars, len);
+            abAppend(ab, &E.row[filerow].chars[E.coloff], len);
         }
 
         abAppend(ab, "\x1b[k", 3);
@@ -244,6 +272,8 @@ void editorDrawRows(struct abuf *ab) {
 }
 
 void editorRefreshScreen() {
+    editorScroll();
+
     struct abuf ab = ABUF_INIT;
 
     abAppend(&ab, "\x1b[?25l", 6);
@@ -252,7 +282,8 @@ void editorRefreshScreen() {
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
+                                              (E.cx - E.coloff) + 1);
     abAppend(&ab, buf, strlen(buf));
     
     abAppend(&ab, "\x1b[?25h", 6);
@@ -271,10 +302,8 @@ void editorMoveCursor(int key) {
           E.cx--;
         }
           break;
-        case ARROW_RIGHT:
-        if (E.cx != E.screencols - 1) {
+        case ARROW_RIGHT:      
           E.cx++;
-        }
           break;
         case ARROW_UP:
         if (E.cy != 0) {
@@ -282,7 +311,7 @@ void editorMoveCursor(int key) {
         }
           break;
         case ARROW_DOWN:
-        if (E.cy != E.screenrows - 1) {
+        if (E.cy < E.numrows) {
           E.cy++;
         }
           break;
@@ -331,7 +360,10 @@ void editorProcessKeypress() {
 void initEditor() {
     E.cx = 0;
     E.cy = 0;
+    E.rowoff = 0;
+    E.coloff = 0;
     E.numrows = 0;
+    E.row = NULL;
 
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
